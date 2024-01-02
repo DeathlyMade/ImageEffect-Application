da5757ff99811c225a788edc32dc34c8eac1d3fe
modaniharsh-iiitb <116881064+modaniharsh-iiitb@users.noreply.github.com>
2024-01-02 22:58:17 +0530

    Update Contrast.cpp

--- ImageEffectBackend/src/Libraries/ContrastLibrary/Contrast.cpp
+++ ImageEffectBackend/src/Libraries/ContrastLibrary/Contrast.cpp
@@ -1,35 +1,22 @@
-#include "Contrast.h"
 #include <vector>
-#include "../Pixel.h"
+#include "Contrast.h"
+
 using namespace std;
 
-void apply_contrast_cpp(vector<vector<Pixel>> &imageVector, float amount) {
+void apply_contrast_cpp(vector<vector<Pixel>> imageVector, float amount) {
     int rows = imageVector.size();
     int cols = imageVector[0].size();
     float minIntensity = 255.0;
     float maxIntensity = 0.0;
 
-    // Finding min and max intensities
-    // for (int i = 0; i < rows; ++i) {
-    //     for (int j = 0; j < cols; ++j) {
-    //         float intensity = (0.299 * imageVector[i][j].r) + (0.587 * imageVector[i][j].g) + (0.114 * imageVector[i][j].b);
-    //         minIntensity = min(minIntensity, intensity);
-    //         maxIntensity = max(maxIntensity, intensity);
-    //     }
-    // }
-
-    // Adjusting the contrast
-    float contrast1=0.5+amount/200;
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            // float intensity = (0.299 * imageVector[i][j].r) + (0.587 * imageVector[i][j].g) + (0.114 * imageVector[i][j].b);
-            // float newIntensity = ((intensity - minIntensity) * amount) + minIntensity;
-            
-
-            // Clamping the values between 0 and 255
-            imageVector[i][j].r = min(255,max(0,static_cast<int>((imageVector[i][j].r-128)*contrast1+128)));
-            imageVector[i][j].g = min(255,max(0,static_cast<int>((imageVector[i][j].g-128)*contrast1+128)));
-            imageVector[i][j].b = min(255,max(0,static_cast<int>((imageVector[i][j].b-128)*contrast1+128)));
+    // Adjusting the contrast to range from 0.5 to 1.5
+    float contrast = 0.5+amount/200;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            // applying the contrast to each pixel and truncating the values from 0 to 255
+            imageVector[i][j].r = min(255,max(0,static_cast<int>((imageVector[i][j].r-128) * contrast + 128)));
+            imageVector[i][j].g = min(255,max(0,static_cast<int>((imageVector[i][j].g-128) * contrast + 128)));
+            imageVector[i][j].b = min(255,max(0,static_cast<int>((imageVector[i][j].b-128) * contrast + 128)));
         }
     }
 }
