19ba788f5acd0c624566c3da252bf59fc01abca7
modaniharsh-iiitb <116881064+modaniharsh-iiitb@users.noreply.github.com>
2024-01-02 23:56:20 +0530

    Fixed contrast

--- ImageEffectBackend/src/Libraries/ContrastLibrary/Contrast.cpp
+++ ImageEffectBackend/src/Libraries/ContrastLibrary/Contrast.cpp
@@ -3,20 +3,17 @@
 
 using namespace std;
 
-void apply_contrast_cpp(vector<vector<Pixel>> imageVector, float amount) {
+void apply_contrast_cpp(vector<vector<Pixel>> &imageVector, float amount) {
     int rows = imageVector.size();
     int cols = imageVector[0].size();
-    float minIntensity = 255.0;
-    float maxIntensity = 0.0;
 
-    // Adjusting the contrast to range from 0.5 to 1.5
-    float contrast = 0.5+amount/200;
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            // applying the contrast to each pixel and truncating the values from 0 to 255
-            imageVector[i][j].r = min(255,max(0,static_cast<int>((imageVector[i][j].r-128) * contrast + 128)));
-            imageVector[i][j].g = min(255,max(0,static_cast<int>((imageVector[i][j].g-128) * contrast + 128)));
-            imageVector[i][j].b = min(255,max(0,static_cast<int>((imageVector[i][j].b-128) * contrast + 128)));
+    // Adjusting the contrast to between 0.5 and 1.5
+    float contrast1 = 0.5+amount/200;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            imageVector[i][j].r = min(255,max(0,static_cast<int>((imageVector[i][j].r-128)*contrast1+128)));
+            imageVector[i][j].g = min(255,max(0,static_cast<int>((imageVector[i][j].g-128)*contrast1+128)));
+            imageVector[i][j].b = min(255,max(0,static_cast<int>((imageVector[i][j].b-128)*contrast1+128)));
         }
     }
 }
